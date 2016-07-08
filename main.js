0cea1bd96fc5ad5a25ec61658f4c091cc303f675
tonio155 <tonio155@gmail.com>
2016-07-08 11:49:19 +0100

    setting up php file

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-console.log("JS Connected!")
+
+// Create the DOM structure and append the first 4 DIVs
+var outlaws = $(".outlaw");
+
+$(function() {
+
+  $('body').append($('<div id="topnav">'));
+  $('body').append($('<div id="container">'));
+  
+  for (n=0; n<5; n++) {
+    $('#container').append($('<div class="outlaw">'));
+  }
+
+// make the system to choose one of the DIVs random and change the color
+//   Math.floor(Math.random() * (.outlaw).length) // this.value
+});
+
+
+
 
