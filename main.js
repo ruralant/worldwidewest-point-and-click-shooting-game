72e453e10710cd8f65c9b864f30eb077529f5497
tonio155 <tonio155@gmail.com>
2016-07-09 21:38:13 +0100

    MVP compleated. Bug with last activated DIV

--- main.js
+++ main.js
@@ -1,93 +1,102 @@
 
 // Create the DOM structure and append the first 4 DIVs
-$(setUp)
+$(function(){
 
-function setUp() {
-  $('body').append($('<button>'));
-  $('body').append($('<div id="topnav">'));
+var $items = $("#gameScreen > div");
 
-  $('button').on('click', playGame);
+$('button').on('click', function() {
+
+  $('#splashScreen').removeClass("hidden");
+  $('#topNav').addClass("hidden");
+  $('#gameScreen').addClass("hidden");
+
+  playGame();
+});
+
+$items.on('click', function() {
+  if ($(this).hasClass("active")) {
+    $(this).removeClass("active");
+    player.score += 100;
+    $('#DisplayPlayerScore').text("Score: " + player.score);
+    console.log("Score " + player.score);
+  };
+});
+
+var playerIndex = 0;
+
+var players = [{
+  score: 0,
+  life: 100
+},{
+  score: 0,
+  life: 100
+}];
+var timerId = 0;
+var player = players[playerIndex];
+
+function gameOver() {
+  clearInterval(timerId);
+  playerIndex++;
+  player = players[playerIndex];
+
+  if(player) {
+    $('#splashScreen').removeClass("hidden");
+    $('#topNav').addClass("hidden");
+    $('#gameScreen').addClass("hidden");
+  } else {
+    checkForWinner();
+  }
 }
 
-var player1 = {
-  Score: 0,
-  Life: 100
-}
-
-var player2 = {
-  Score: 0,
-  Life: 100
-}
-
-function player1Turn(player1) {
-  playGame(player1);
-  console.log("Player 1 Score: " + player1.playerScore);
-}
 
 function playGame() {
-  $('<button>').remove();
-  $('body').append($('<div id="screen">'));
-  $('#topnav').append($('<form class="scoreBoard" id="DisplayPlayerScore">'));
-  $('#topnav').append($('<form class="scoreBoard" id="DisplayPlayerLife">'));
-  $('<button>').toggleClass("hidden");
 
-  for (n=0; n<20; n++) {
-    $('#screen').append($('<div id="div' + n +'">')); // create divs with different //IDs style="background-color:red"
-  }
-  var $items = $("#screen > div");
-  var timerId = setInterval(createRandomItem, 1000);
+  $('#splashScreen').addClass("hidden");
+  $('#topNav').removeClass("hidden");
+  $('#gameScreen').removeClass("hidden");
+  timerId = setInterval(createRandomItem, 1000);
+
+  $('#DisplayPlayerScore').text("Score: " + player.score);
+  $('#DisplayPlayerLife').text("Life:" + player.life);
+
   setTimeout(function() {
-    clearInterval(timerId);
-
-    // if (playGame(player1)) {
-    //   playGame(player2);
-    // } else {
-    // checkForWinner();
-    // console.log(timerId)
-    // };
-
-  }, 60*1000); // 60sec
-  var playerScore = 0;
-  var playerLife = 100;
+    gameOver();
+  }, 30*1000); // 10sec
   
-  function createRandomItem() {
+  function createRandomItem() {                                                   // check for the bug
     var $randomItem = $items.eq(Math.floor(Math.random() * $items.length));
-    if ($($items).hasClass("")) {
+    if ($randomItem.hasClass("")) {
       $randomItem.toggleClass("active");
       setTimeout(function() {
         if($randomItem.hasClass("active")) {
           $randomItem.removeClass("active");
-          playerLife -= 10;
-          $('#DisplayPlayerLife').text("Life:" + playerLife);
-          console.log("Life " + playerLife);
-          // check for dead ---> if life = 0, start player 2!
-            if (player1.Life === 0) {
-              //start player2;
-            } else {
-              //checkForWinner
-            }
+          player.life -= 10;
+          $('#DisplayPlayerLife').text("Life:" + player.life);
+          if(player.life === 0) {
+            // stop(createRandomItem);
+            gameOver();
+          }
         }
-      }, 2000);
+      }, 1000);
     } 
-    clickedItem($randomItem);
-  }
-
-  function clickedItem($randomItem) {
-    $($randomItem).on('click', function() {
-      if ($(this).hasClass("active")) {
-          $(this).removeClass("active");
-          playerScore += 100;
-          $('#DisplayPlayerScore').text("Score: " + playerScore);
-          console.log("Score " + playerScore);
-      };
-    });  
+    setTimeout(function() {
+    }, 28*1000); // 10sec
   }
 }
 
 function checkForWinner() {
-  if (player1Score > player2Score) {
-    alert("Player 1 Wins!");
+  $('#splashScreen').addClass("hidden");
+  $('#winner').removeClass("hidden");
+  $('button').addClass("hidden");
+  $('#topNav').removeClass("hidden");
+  $('#gameScreen').removeClass("hidden");
+  if (players[0].score > players[1].score) {
+    $('#winner').text("Player 1 Wins!");
+    //alert("Player 1 Wins!");
   } else {
-    alert("Player 2 Wins");
+    $('#winner').text("Player 1 Wins!");
   }
+  // $(start)
 }
+
+});
